610aafb907482ea2466aa5dc8830b9bd321f146e
nyaxk <contact@nyaxk.com>
2025-03-17 12:49:38 -0300

    fix

--- src/BoltServiceProvider.php
+++ src/BoltServiceProvider.php
@@ -73,7 +73,7 @@ class BoltServiceProvider extends PackageServiceProvider
             'add_grade_to_response',
             'add_grade_to_field_response',
             'add_borderless_to_section',
-            'add_is_template_to_forms'
+            'add_is_template_to_form'
         ];
     }
 }
